8d12727843d5afd1c151dc20ac6f0d66b06e3b04
SamuelSvahn <samuelsvahn@hotmail.com>
2023-03-29 09:40:37 +0200

    React workshop 2, part 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import { useForm } from 'react-hook-form';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from 'axios'; 
+
+
+
+const greetingMessage = <div>Hello React!</div>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(greetingMessage);
